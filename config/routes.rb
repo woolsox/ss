2ca34d5eb2baaa7ef6abed1e0cf0b0ee6f15d11f
woolsox <dimeo.colin@gmail.com>
2018-08-01 23:01:10 -0400

    adds dash tiles/icons, root route, styles and other misc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, controller: "dashboard"
 
-  # root to: "/"
+  root to: "dashboard#show"
   ActiveAdmin.routes(self)
 end
